1ee8ab195927f25bf956469df10ac4d49aec9cb9
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2021-05-02 21:59:12 +0300

    docs: added scope description

--- packages/common/lib/interfaces/scope.interface.ts
+++ packages/common/lib/interfaces/scope.interface.ts
@@ -1,8 +1,17 @@
 export enum Scope {
   /**
+   * Singleton objects are the same for every request.
    * @default
    */
   SINGLETON = 'singleton',
+
+  /**
+   * Transient objects are always different.
+   */
   TRANSIENT = 'transient',
+
+  /**
+   * Request objects are the same for each request but different across each request.
+   */
   REQUEST = 'request',
 }
