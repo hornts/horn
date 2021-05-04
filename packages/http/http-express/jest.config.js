ec8e81ceaae3f77574b19c6feedf3a7aad0798f6
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2021-05-04 23:12:01 +0300

    chore: fixed coverage collection

--- packages/http/http-express/jest.config.js
+++ packages/http/http-express/jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/http/http-express',
+  coverageDirectory: '../../../coverage/packages/http/http-express',
 };
--- packages/http/http-fastify/jest.config.js
+++ packages/http/http-fastify/jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/http/http-fastify',
+  coverageDirectory: '../../../coverage/packages/http/http-fastify',
 };
