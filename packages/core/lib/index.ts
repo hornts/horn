972861e594118867c59543c223baa314d4add378
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2021-05-07 01:08:36 +0300

    chore: added circular dependency error

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -2,5 +2,6 @@ import 'reflect-metadata';
 
 export * from './di';
 export * from './http';
+export * from './errors';
 export { HornApplication, HornOptions } from './horn.app';
 export { HornFactory } from './horn.factory';
