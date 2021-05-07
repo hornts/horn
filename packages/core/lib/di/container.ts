c466a615a74d820208f61626acdc1e0b28406cf0
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2021-05-07 15:40:08 +0300

    chore: updated load injectables

--- packages/core/lib/di/container.ts
+++ packages/core/lib/di/container.ts
@@ -35,10 +35,12 @@ export class ApplicationContainer {
 
     this.logger?.info('Application container started.');
 
-    this.logger?.debug(this.graph.dependenciesOf('module:AppModule').join(', '));
-    this.logger?.debug(this.graph.dependenciesOf('module:ModuleA').join(', '));
-    this.logger?.debug(this.graph.dependenciesOf('module:ModuleB').join(', '));
-    this.logger?.debug(this.graph.dependenciesOf('module:ModuleC').join(', '));
+    this.logger?.debug(
+      `module:AppModule: ${this.graph.dependenciesOf('module:AppModule').join(', ')}`
+    );
+    this.logger?.debug(`module:ModuleA: ${this.graph.dependenciesOf('module:ModuleA').join(', ')}`);
+    this.logger?.debug(`module:ModuleB: ${this.graph.dependenciesOf('module:ModuleB').join(', ')}`);
+    this.logger?.debug(`module:ModuleC: ${this.graph.dependenciesOf('module:ModuleC').join(', ')}`);
   }
 
   private instantiateDependencies(ref: Type<any>) {
@@ -89,14 +91,17 @@ export class ApplicationContainer {
   private loadModuleInjectables(module: Module, { injectables }: ModuleOptions) {
     if (Array.isArray(injectables)) {
       for (let index = 0; index < injectables.length; index++) {
-        // TODO: first try to get from graph
         const injectable = new Instance(injectables[index]);
+
         const token = injectable.getToken();
 
-        this.graph.addNode(token, injectable);
-        this.graph.addDependency(module.getToken(), token);
+        if (!this.graph.hasNode(token)) {
+          this.graph.addNode(token, injectable);
 
-        this.loadInjectableParams(injectable);
+          this.loadInjectableParams(injectable);
+        }
+
+        this.graph.addDependency(module.getToken(), token);
       }
     }
   }
