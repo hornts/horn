ec6cb0dbc165086a1abf12c9431a8334518c304e
Alexey Vasyukov <vasyukov.alexey.v@gmail.com>
2021-09-06 00:24:22 +0300

    fix: graph build

--- packages/core/lib/di/dependency-graph/dependency-graph.ts
+++ packages/core/lib/di/dependency-graph/dependency-graph.ts
@@ -1,4 +1,4 @@
-import { ModuleOptions, Type } from '@hornts/common';
+import { Type } from '@hornts/common';
 import { DepGraph } from 'dependency-graph';
 
 import { Controller, Injectable } from '../injectable';
@@ -24,41 +24,66 @@ export class DependencyGraph {
 
     this.graph.addNode(token, new Node(module));
 
-    this.loadInjectables(token, meta);
-    this.loadControllers(token, meta);
+    this.loadInjectables(token, meta.injectables);
+    this.loadControllers(token, meta.controllers);
 
     for (let index = 0; index < meta.imports.length; index++) {
-      if (!this.hasModule(meta.imports[index])) {
+      const importedModuleToken = this.extractModuleToken(meta.imports[index]);
+
+      if (!this.graph.hasNode(importedModuleToken)) {
         this.loadDependencies(meta.imports[index]);
       }
+
+      this.graph.addDependency(token, importedModuleToken);
     }
   }
 
-  private loadInjectables(rootToken: string, { injectables }: ModuleOptions) {
+  private loadInjectables(rootToken: string, injectables: Type<any>[]) {
     for (let index = 0; index < injectables.length; index++) {
       const injectable = new Injectable(injectables[index]);
 
       const token = injectable.getToken();
 
-      this.graph.addNode(token, new Node(injectable));
+      if (!this.hasInjectable(injectables[index])) {
+        this.graph.addNode(token, new Node(injectable));
+        this.loadInjectables(token, injectable.getDependencies());
+      }
+
       this.graph.addDependency(rootToken, token);
     }
   }
 
-  private loadControllers(rootToken: string, { controllers }: ModuleOptions) {
+  private loadControllers(rootToken: string, controllers: Type<any>[]) {
     for (let index = 0; index < controllers.length; index++) {
       const controller = new Controller(controllers[index]);
 
       const token = controller.getToken();
 
-      this.graph.addNode(token, new Node(controller));
+      if (!this.hasController(controllers[index])) {
+        this.graph.addNode(token, new Node(controller));
+        this.loadInjectables(token, controller.getDependencies());
+      }
+
       this.graph.addDependency(rootToken, token);
     }
   }
 
-  private hasModule(ref: Type<any>): boolean {
+  private extractModuleToken(ref: Type<any>): string {
     const module = new Module(ref);
-    const token = module.getToken();
+
+    return module.getToken();
+  }
+
+  private hasInjectable(ref: Type<any>): boolean {
+    const injectable = new Injectable(ref);
+    const token = injectable.getToken();
+
+    return this.graph.hasNode(token);
+  }
+
+  private hasController(ref: Type<any>): boolean {
+    const controller = new Controller(ref);
+    const token = controller.getToken();
 
     return this.graph.hasNode(token);
   }
